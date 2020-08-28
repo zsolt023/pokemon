681cbeb2a6d4e460aedc90fffc8dbe6fe5cd4158
zsolt023 <zsolt023.bme.vik@gmail.com>
2020-08-28 09:14:40 +0200

    added base project

---
+++ pokemon-android/app/src/main/java/nagy/zsolt/pokemon_android/MainActivity.kt
@@ -0,0 +1,11 @@
+package nagy.zsolt.pokemon_android
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
